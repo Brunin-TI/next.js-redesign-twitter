0546cc08be891c8346e951de0737f0a3f0e986a1
BruninTI <bruno.araujo@viannasempre.com.br>
2022-08-01 08:23:41 -0300

    feat: improvements on box component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-export interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
+interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
 }
 
-const Box: React.FC<BoxProps> = ({ children, className, ...props }) => {
+export const Box: React.FC<BoxProps> = ({ children, className, ...props }) => {
   return (
     <div className={className} {...props}>
       {children}
     </div>
   );
 };
-
-export default Box;
